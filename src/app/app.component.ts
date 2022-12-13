4f1f75640deee5039b61ad76be79a7dd2a3dfc53
Alireza2794 <alirezahaghshenas126@gmail.com>
2022-12-13 18:42:02 +0330

    update header and side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
-    { title: 'داشبورد', url: '/folder/dashboard', Image: './assets/icons/dashboard-5479.png' },
-    { title: 'لیست دانشجویان', url: '/folder/student-list', Image: './assets/icons/user-268.png'  },
-    { title: 'ارسال پیام در کارتابل', url: '/folder/cartable', Image: './assets/icons/send-mail-2574.png'  },
-    { title: 'نقشه دانشگاه', url: '/folder/map-university', Image: './assets/icons/maps-location-11094_1.png'  },
-    { title: 'بنرها و تبلیغات', url: '/folder/banners', Image: './assets/icons/search-1163.png'  }
+    { title: 'داشبورد', url: '/folder/dashboard', Image: './assets/icons/Framedashboard.png', Image2: './assets/icons/Framedashboard2.png' },
+    { title: 'لیست دانشجویان', url: '/folder/student', Image: './assets/icons/Framelist.png', Image2: './assets/icons/Framelist2.png'  },
+    { title: 'ارسال پیام در کارتابل', url: '/folder/message', Image: './assets/icons/Iconcartable.png', Image2: './assets/icons/Iconcartable2.png'  },
+    { title: 'نقشه دانشگاه', url: '/folder/map-university', Image: './assets/icons/Framemap.png'  },
+    { title: 'بنرها و تبلیغات', url: '/folder/banners', Image: './assets/icons/Framebanner.png'  }
   ];
   constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
 }
